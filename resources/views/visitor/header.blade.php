06c91a158088111918ff4e4977b088a9fd1f5317
Firoz Ebna Jobaier <firoz.jobaier@gmail.com>
2023-03-21 20:49:14 +0600

    21.03.23

---
+++ resources/views/visitor/header.blade.php
@@ -0,0 +1,67 @@
+<header class="common-header">
+    <nav class="navbar">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/"><img src="images/home-1/logo.png" alt="Logo"></a>
+            </div>
+
+            <div id="navbar" class="pull-left collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active dropdown">
+                        <a href="" class="dropdown-toggle" data-toggle="dropdown">Home <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="index-2.html">Style 2</a></li>
+                            <li><a href="index-3.html">Style 3</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Vcitoria Shop <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="shop.html">Shop</a></li>
+                            <li><a href="shop-details.html">Shop Details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="service.html">Services</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Photo Gallery <i class="fa fa-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="gallery.html">Gallery 1</a></li>
+                            <li><a href="gallery-2.html">Gallery 2</a></li>
+                            <li><a href="gallery-3.html">Gallery 3</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="team.html">Team</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="search-wrapper pull-right">
+                <ul>
+                    <li>
+                        <button class="open-menu"><i class="fa fa-bars"></i></button>
+                    </li>
+
+                    <li>
+                        <button class="open-search"><i class="fa fa-search"></i></button>
+                        <ul class="search-box">
+                            <li>
+                                <form action="#">
+                                    <input type="text" placeholder="Search....">
+                                    <button type="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('visitor/landing');
+});
+
+Route::get('/shop', function () {
+    return view('visitor/shop');
 });
