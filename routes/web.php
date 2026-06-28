a5005cbbe21f6aec30fc63a721c562cc771d0b7f
International Climate Finance Cell <icfcerdbd@gmail.com>
2026-06-28 12:28:51 +0600

    Remove unused lines

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// ── Public Routes ──────────────────────────────────────────
 Route::get('/', function () {
     return view('visitor.pages.index');
 })->name('home');
